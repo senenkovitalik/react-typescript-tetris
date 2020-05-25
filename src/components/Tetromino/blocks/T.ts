95b877277a26cc86954b271e254bce6d0655d8ee
Vitaliy Senenko <senenkovitalik@gmail.com>
2020-05-25 19:27:01 +0300

    feat: T left rotate

--- src/components/Tetromino/blocks/T.ts
+++ src/components/Tetromino/blocks/T.ts
@@ -60,6 +60,53 @@ export default class T extends Tetromino {
   }
 
   rotateLeft(): void {
+    switch (this.orientation) {
+      // case letter mean current orientation
+      case 'N': {
+        const [left, center, right, bottom] = this.coords;
+        this.coords = [
+          {
+            row: left.row - 1,
+            col: left.col + 1
+          },
+          center,
+          right,
+          bottom
+        ]; // U C R B
+        break;
+      }
+      case 'W': {
+        const [up, center, right, bottom] = this.coords;
+        this.coords = [
+          up,
+          {row: bottom.row - 1, col: bottom.col - 1},
+          center,
+          right,
+        ]; // U L C R
+        break;
+      }
+      case 'S': {
+        const [up, left, center, right] = this.coords;
+        this.coords = [
+          up,
+          left,
+          center,
+          {row: right.row + 1, col: right.col - 1}
+        ]; // U L C B
+        break;
+      }
+      case 'E': {
+        const [up, left, center, bottom] = this.coords;
+        this.coords = [
+          left,
+          center,
+          {row: up.row + 1, col: up.col + 1},
+          bottom
+        ]; // U L C B
+        break;
+      }
+    }
 
+    this.orientation = SIDES[this.orientation].left;
   }
 }
