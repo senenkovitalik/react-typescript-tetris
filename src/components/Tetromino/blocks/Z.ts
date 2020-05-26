6f21769f939f7a187344b8564aa2f07bd0bc6740
Vitaliy Senenko <senenkovitalik@gmail.com>
2020-05-26 13:05:34 +0300

    feat: Z rotation

--- src/components/Tetromino/blocks/Z.ts
+++ src/components/Tetromino/blocks/Z.ts
@@ -1,4 +1,5 @@
 import Tetromino from '../Tetromino';
+import {SIDES} from '../constants';
 
 export default class Z extends Tetromino {
   constructor(initCol: number, numCols: number) {
@@ -12,8 +13,76 @@ export default class Z extends Tetromino {
       {row: -1, col: this.startCol + 2},
     ];
   }
-  
-  rotate(): void {
 
+  rotate(): void {
+    switch (this.orientation) {
+      // case letter mean current orientation
+      case 'N': {
+        const [left, centerUp, centerBottom, right] = this.coords;
+        this.coords = [
+          {
+            row: centerUp.row,
+            col: centerUp.col + 1
+          },
+          centerBottom,
+          right,
+          {
+            row: left.row + 2,
+            col: left.col + 1
+          }
+        ]; // U CL CR B
+        break;
+      }
+      case 'E': {
+        const [up, centerLeft, centerRight, bottom] = this.coords;
+        this.coords = [
+          {
+            row: up.row,
+            col: up.col - 2
+          },
+          {
+            row: bottom.row - 2,
+            col: bottom.col
+          },
+          centerLeft,
+          centerRight,
+        ]; // L CU CB R
+        break;
+      }
+      case 'S': {
+        const [left, centerUp, centerBottom, right] = this.coords;
+        this.coords = [
+          centerUp,
+          {
+            row: left.row + 1,
+            col: left.col
+          },
+          centerBottom,
+          {
+            row: right.row + 1,
+            col: right.col - 2
+          }
+        ]; // U CL CR B
+        break;
+      }
+      case 'W': {
+        const [up, centerLeft, centerRight, bottom] = this.coords;
+        this.coords = [
+          {
+            row: centerLeft.row - 1,
+            col: centerLeft.col
+          },
+          up,
+          centerRight,
+          {
+            row: bottom.row - 1,
+            col: bottom.col + 2
+          }
+        ]; // U CL CR B
+        break;
+      }
+    }
+
+    this.orientation = SIDES[this.orientation];
   }
 }
