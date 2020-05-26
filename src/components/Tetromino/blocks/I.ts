6f96763bebbb9497401c98fb6dd580c146d8ae8e
Vitaliy Senenko <senenkovitalik@gmail.com>
2020-05-26 17:04:38 +0300

    feat: I rotation

--- src/components/Tetromino/blocks/I.ts
+++ src/components/Tetromino/blocks/I.ts
@@ -1,5 +1,6 @@
 import Tetromino from '../Tetromino';
 import Coordinate from '../../Coordinate/Coordinate';
+import {SIDES} from '../constants';
 
 export default class I extends Tetromino {
   constructor(initCol: number, numCols: number) {
@@ -15,6 +16,50 @@ export default class I extends Tetromino {
   }
 
   rotate(): void {
+    switch (this.orientation) {
+      // case letter mean current orientation
+      case 'N': {
+        const [left, centerLeft, centerRight, right] = this.coords;
+        this.coords = [
+          left.right(2).up(2),
+          centerLeft.right().up(),
+          centerRight,
+          right.left().down()
+        ];
+        break;
+      }
+      case 'E': {
+        const [up, centerUp, centerBottom, bottom] = this.coords;
+        this.coords = [
+          up.left(2).down(),
+          centerUp.left(),
+          centerBottom.up(),
+          bottom.up(2).right()
+        ];
+        break;
+      }
+      case 'S': {
+        const [left, centerLeft, centerRight, right] = this.coords;
+        this.coords = [
+          left.right().up(),
+          centerLeft,
+          centerRight.left().down(),
+          right.left(2).down(2)
+        ]; // U C R B
+        break;
+      }
+      case 'W': {
+        const [up, centerUp, centerBottom, bottom] = this.coords;
+        this.coords = [
+          up.left(1).down(),
+          centerUp,
+          centerBottom.up().right(),
+          bottom.up(2).right(2)
+        ]; // L C R B
+        break;
+      }
+    }
 
+    this.orientation = SIDES[this.orientation];
   }
 }
