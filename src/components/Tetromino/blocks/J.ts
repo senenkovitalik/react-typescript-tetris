59d1456054660c54ad2522cd4af48aa05da7bd5d
Vitaliy Senenko <senenkovitalik@gmail.com>
2020-05-26 17:24:54 +0300

    feat: J rotation

--- src/components/Tetromino/blocks/J.ts
+++ src/components/Tetromino/blocks/J.ts
@@ -1,5 +1,6 @@
 import Tetromino from '../Tetromino';
 import Coordinate from '../../Coordinate/Coordinate';
+import {SIDES} from '../constants';
 
 export default class J extends Tetromino {
   constructor(initCol: number, numCols: number) {
@@ -15,6 +16,50 @@ export default class J extends Tetromino {
   }
 
   rotate(): void {
+    switch (this.orientation) {
+      // case letter mean current orientation
+      case 'N': {
+        const [leftUp, leftDown, center, right] = this.coords;
+        this.coords = [
+          leftUp.right(1),
+          leftDown.right(2).up(),
+          center,
+          right.down().left()
+        ];
+        break;
+      }
+      case 'E': {
+        const [upLeft, upRight, center, bottom] = this.coords;
+        this.coords = [
+          upLeft.left().down(),
+          center,
+          upRight.down(),
+          bottom.right()
+        ];
+        break;
+      }
+      case 'S': {
+        const [left, center, right, rightBottom] = this.coords;
+        this.coords = [
+          left.up().right(),
+          center,
+          right.left().down(),
+          rightBottom.left(2)
+        ];
+        break;
+      }
+      case 'W': {
+        const [up, center, bottom, bottomLeft] = this.coords;
+        this.coords = [
+          up.left(),
+          bottomLeft.up(),
+          center,
+          bottom.up().right()
+        ];
+        break;
+      }
+    }
 
+    this.orientation = SIDES[this.orientation];
   }
 }
